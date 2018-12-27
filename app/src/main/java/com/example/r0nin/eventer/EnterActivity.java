9aaaac110aa84a189205fae239ea298cc7814734
Kamil-Piotrowski <kamil.piotrowski01@student.wat.edu.pl>
2018-12-27 12:32:51 +0100

    git test

--- app/src/main/java/com/example/r0nin/eventer/EnterActivity.java
+++ app/src/main/java/com/example/r0nin/eventer/EnterActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class EnterActivity extends AppCompatActivity {
-
+//git test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
