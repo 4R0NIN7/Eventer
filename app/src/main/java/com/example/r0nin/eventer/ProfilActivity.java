f83beb6cda1b852925a26b649bf59d56a4cc3fc4
Kamil-Piotrowski <kamil.piotrowski01@student.wat.edu.pl>
2018-12-27 18:07:32 +0100

    get

--- app/src/main/java/com/example/r0nin/eventer/ProfilActivity.java
+++ app/src/main/java/com/example/r0nin/eventer/ProfilActivity.java
@@ -29,7 +29,6 @@ public class ProfilActivity extends AppCompatActivity {
     TextView textViewDataUrodzenia;
     TextView textViewTelefon;
     Button btnDodaj;
-    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,68 +47,35 @@ public class ProfilActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                id = 1;
-                List<Object> params = new ArrayList<Object>(){ };
-                params.add(id);
-                params.add(textViewLogin);
-                params.add(textViewPunkty);
-                params.add(textViewImie);
-                params.add(textViewNazwisko);
-                params.add(textViewDataUrodzenia);
-                params.add(textViewTelefon);
-                new FetchUserDataTask().execute(params);
-                /*AsyncTask task = new AsyncTask(){
+                Integer i = 1;
+                String s = "test";
+                loadUserData(s);
 
-                    @Override
-                    protected Object doInBackground(Object[] objects) {
-                        OkHttpClient client = new OkHttpClient();
-                        Request req = new Request.Builder().url("http://192.168.198.1:51000/api/Uzytkownik/2").build();
-                        Response res = null;
-                        try{
-                            res = client.newCall(req).execute();
-                            Log.d("godzilla", "test1");
-                            return res.body().string();
-                        }catch(IOException exception){
-                            exception.printStackTrace();
-                        }
-                        return null;
-                    }
-                    @Override
-                    protected void onPostExecute(Object o){
-                        try {
-                            String JSONstring = o.toString();
-                            Log.d("godzilla", "jsonstring: "+JSONstring);
-                            JSONArray json = new JSONArray(o.toString());
-                            Log.d("godzilla", "jsonobject: "+json.toString());
-                            JSONObject userJson = json.getJSONObject(0);
-
-
-                            String login = userJson.getString("login");
-                            Log.d("godzilla", "login: "+login);
-                            textViewLogin.setText(userJson.getString("login"));
-                            textViewPunkty.setText(userJson.getString("punkty"));
-                            textViewImie.setText(userJson.getString("imie"));
-                            textViewNazwisko.setText(userJson.getString("nazwisko"));
-                            textViewDataUrodzenia.setText(userJson.getString("data_urodzenia"));
-                            textViewTelefon.setText(userJson.getString("nr_telefonu"));
-                        } catch (JSONException e) {
-                            Log.d("godzilla", "error");
-                        }
-
-
-                    }
-                }.execute();*/
             }
         });
     }
 
-    private class FetchUserDataTask extends AsyncTask<List<Object>, Void, Object>{
+    private void loadUserData(Object id) {
+        new FetchUserDataTask().execute(id);
+    }
+
+    private class FetchUserDataTask extends AsyncTask<Object, Void, Object>{
+
+
 
         @Override
-        protected Object doInBackground(List<Object>... params) {
+        protected Object doInBackground(Object... params) {
 
             OkHttpClient client = new OkHttpClient();
-            Request req = new Request.Builder().url("http://192.168.198.1:51000/api/Uzytkownik/2").build();
+            Object IdOrLogin = params[0];
+            Request req;
+            try{
+                Integer id = (Integer)IdOrLogin;
+                req = new Request.Builder().url("http://192.168.198.1:51000/api/Uzytkownik/"+id).build();
+            }catch(Exception e){
+                String login = IdOrLogin.toString();
+                req = new Request.Builder().url("http://192.168.198.1:51000/api/Uzytkownik/login/"+login).build();
+            }
             Response res = null;
             try{
                 res = client.newCall(req).execute();
