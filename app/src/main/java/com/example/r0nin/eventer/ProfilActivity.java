fee6e3d67c1b1f82bd5c5c2aa32d8542032f622d
Kamil-Piotrowski <kamil.piotrowski01@student.wat.edu.pl>
2018-12-27 16:40:16 +0100

    gotowy get danych użytkownika (id na razie na sztywno)

--- app/src/main/java/com/example/r0nin/eventer/ProfilActivity.java
+++ app/src/main/java/com/example/r0nin/eventer/ProfilActivity.java
@@ -8,10 +8,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.*;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -26,6 +29,7 @@ public class ProfilActivity extends AppCompatActivity {
     TextView textViewDataUrodzenia;
     TextView textViewTelefon;
     Button btnDodaj;
+    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +48,17 @@ public class ProfilActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                AsyncTask task = new AsyncTask(){
+                id = 1;
+                List<Object> params = new ArrayList<Object>(){ };
+                params.add(id);
+                params.add(textViewLogin);
+                params.add(textViewPunkty);
+                params.add(textViewImie);
+                params.add(textViewNazwisko);
+                params.add(textViewDataUrodzenia);
+                params.add(textViewTelefon);
+                new FetchUserDataTask().execute(params);
+                /*AsyncTask task = new AsyncTask(){
 
                     @Override
                     protected Object doInBackground(Object[] objects) {
@@ -65,29 +79,65 @@ public class ProfilActivity extends AppCompatActivity {
                         try {
                             String JSONstring = o.toString();
                             Log.d("godzilla", "jsonstring: "+JSONstring);
-                            JSONObject json = new JSONObject(o.toString());
-                            String login = json.getString("login");
+                            JSONArray json = new JSONArray(o.toString());
+                            Log.d("godzilla", "jsonobject: "+json.toString());
+                            JSONObject userJson = json.getJSONObject(0);
+
+
+                            String login = userJson.getString("login");
                             Log.d("godzilla", "login: "+login);
-                            textViewLogin.setText(json.getString("login"));
-                            textViewPunkty.setText(json.getString("punkty"));
-                            textViewImie.setText(json.getString("imie"));
-                            textViewNazwisko.setText(json.getString("nazwisko"));
-                            textViewDataUrodzenia.setText(json.getString("data_urodzenia"));
-                            textViewTelefon.setText(json.getString("nr_telefonu"));
+                            textViewLogin.setText(userJson.getString("login"));
+                            textViewPunkty.setText(userJson.getString("punkty"));
+                            textViewImie.setText(userJson.getString("imie"));
+                            textViewNazwisko.setText(userJson.getString("nazwisko"));
+                            textViewDataUrodzenia.setText(userJson.getString("data_urodzenia"));
+                            textViewTelefon.setText(userJson.getString("nr_telefonu"));
                         } catch (JSONException e) {
                             Log.d("godzilla", "error");
                         }
 
 
                     }
-                }.execute();
+                }.execute();*/
             }
         });
     }
 
-    private void getData(){
+    private class FetchUserDataTask extends AsyncTask<List<Object>, Void, Object>{
 
+        @Override
+        protected Object doInBackground(List<Object>... params) {
+
+            OkHttpClient client = new OkHttpClient();
+            Request req = new Request.Builder().url("http://192.168.198.1:51000/api/Uzytkownik/2").build();
+            Response res = null;
+            try{
+                res = client.newCall(req).execute();
+                return res.body().string();
+            }catch(IOException exception){
+                exception.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            try {
+                if(o != null){
+                JSONArray json = new JSONArray(o.toString());
+                JSONObject userJson = json.getJSONObject(0);
+                textViewLogin.setText("Login: " + userJson.getString("login"));
+                textViewPunkty.setText("Punkty: "+userJson.getString("punkty"));
+                textViewImie.setText("Imię: "+userJson.getString("imie"));
+                textViewNazwisko.setText("Nazwisko: "+userJson.getString("nazwisko"));
+                textViewDataUrodzenia.setText("Data urodzenia: "+userJson.getString("data_urodzenia"));
+                textViewTelefon.setText("Telefon: "+userJson.getString("nr_telefonu"));
+                }
+            } catch (JSONException e) {
+
+            }
+        }
     }
-
 
 }
