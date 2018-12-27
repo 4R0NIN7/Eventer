c315b7f825bedd96e0b62067c01a7076f3d85c32
Kamil-Piotrowski <kamil.piotrowski01@student.wat.edu.pl>
2018-12-27 12:39:10 +0100

    onMapClickListener

--- app/src/main/java/com/example/r0nin/eventer/MapsActivity.java
+++ app/src/main/java/com/example/r0nin/eventer/MapsActivity.java
@@ -42,5 +42,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //doMapClick(latLng);
+            }
+        });
     }
+
+
 }
